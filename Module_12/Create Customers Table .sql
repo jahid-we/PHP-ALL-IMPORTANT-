bea84ce8acb3cdb828f53ace4cd1efe1600475ee
Jahid Hasan <jahidhasan370919@gmail.com>
2024-05-30 20:15:30 +0600

    ok

--- Module_12/Create Customers Table .sql
+++ Module_12/Create Customers Table .sql
@@ -3,7 +3,7 @@ CREATE TABLE `customers`(
     `FirstName` VARCHAR(50) NOT NULL,
     `Email` VARCHAR(50) NOT NULL,
     `Mobile` VARCHAR(50) NOT NULL,
-    `user_id` BIGINT(20) UNIQUE NOT NULL,
+    `user_id` BIGINT(20) UNSIGNED NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT Current_timestamp(),
     `updated_at`TIMESTAMP NOT NULL DEFAULT Current_timestamp() on UPDATE Current_timestamp(),
     FOREIGN KEY(`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE
